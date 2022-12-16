ca92e07ec6637ad25e6a179237c6a4142f42a9e0
Christian Sonntag <info@motions-media.de>
2022-12-16 16:57:41 +0100

    fix path

--- public/templates/node.html.php
+++ public/templates/node.html.php
@@ -5,14 +5,14 @@
     <meta name="keywords" content="<?= $node->keywords ?? 'default,keywords' ?>">
     <meta name="description" content="<?= $node->description ?? 'Default description' ?>">
 
-<?php require_once 'templates/partials/resources.header.html.php'; ?>
+<?php require_once 'partials/resources.header.html.php'; ?>
 
 </head>
 <body>
 
 <?= $node->body ?? 'EMPTY?' ?>
 
-<?php require_once 'templates/partials/resources.footer.html.php'; ?>
+<?php require_once 'partials/resources.footer.html.php'; ?>
 
 </body>
 </html>
